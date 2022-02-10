89a7977aad56d775a574678428e566d07a837f54
kahlilthabet <khalilthabet@outlook.com>
2022-02-10 23:59:22 +0100

    88. Merge Sorted Array

---
+++ 88.merge-sorted-array.cpp
@@ -0,0 +1,22 @@
+class Solution
+{
+public:
+    void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
+    {
+        int pointerOne = n - 1;
+        int pointerTwo = m - 1;
+        int end = nums1.size() - 1;
+        while (pointerOne >= 0 && pointerTwo >= 0)
+        {   
+            if (nums1[pointerOne] > nums2[pointerTwo])
+            {
+                nums1[end--] = nums1[pointerOne--];
+            }
+            else if (nums1[pointerOne] <= nums2[pointerTwo])
+            {
+                nums1[end--] = nums2[pointerTwo--];
+            }
+        }
+
+    }
+};
